05abf57356fd870a9888fbbe830bb3b74e5c2eb7
WunderwaffleX <88868486+WunderwaffleX@users.noreply.github.com>
2022-05-22 14:57:03 +0300

    Delete states directory

--- states/city.py
+++
@@ -1,4 +0,0 @@
-from aiogram.dispatcher.filters.state import StatesGroup, State
-
-class Select_City(StatesGroup):
-    city_set = State()
--- states/datetime.py
+++
@@ -1,4 +0,0 @@
-from aiogram.dispatcher.filters.state import StatesGroup, State
-
-class Select_Time(StatesGroup):
-    time_set = State()
